51b98ac36dcf0ff263354a78139029d26604d5f8
MatheusZanivan <zanivanandrade@gmail.com>
2022-05-04 14:09:52 -0300

    buttons referenced

--- Tabuando/Tabuando/Telas/Game1ViewController.swift
+++ Tabuando/Tabuando/Telas/Game1ViewController.swift
@@ -14,17 +14,12 @@ class Game1ViewController: UIViewController {
     private weak var desafioLabel: UILabel?
     private weak var igualLabel: UILabel?
     private weak var resultadoView: UIView?
-    private weak var resposta1View: UIView?
-    private weak var resposta1Label: UILabel?
-    private weak var resposta2View: UIView?
-    private weak var resposta2Label: UILabel?
-    private weak var resposta3View: UIView?
-    private weak var resposta3Label: UILabel?
-    private weak var resposta4View: UIView?
-    private weak var resposta4Label: UILabel?
+    private weak var resposta1Button: UIButton?
+    private weak var resposta2Button: UIButton?
+    private weak var resposta3Button: UIButton?
+    private weak var resposta4Button: UIButton?
     private weak var centralizadorView: UIView?
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,14 +29,10 @@ class Game1ViewController: UIViewController {
         let desafioLabel = UILabel()
         let igualLabel = UILabel()
         let resultadoView = UIView()
-        let resposta1View = UIView()
-        let resposta1Label = UILabel()
-        let resposta2View = UIView()
-        let resposta2Label = UILabel()
-        let resposta3View = UIView()
-        let resposta3Label = UILabel()
-        let resposta4View = UIView()
-        let resposta4Label = UILabel()
+        let resposta1Button = UIButton()
+        let resposta2Button = UIButton()
+        let resposta3Button = UIButton()
+        let resposta4Button = UIButton()
         let centralizadorView = UIView()
         
        
@@ -52,14 +43,14 @@ class Game1ViewController: UIViewController {
         desafioView.addSubview(desafioLabel)
         desafioView.addSubview(igualLabel)
         desafioView.addSubview(resultadoView)
-        self.view.addSubview(resposta1View)
-        resposta1View.addSubview(resposta1Label)
-        self.view.addSubview(resposta2View)
-        resposta2View.addSubview(resposta2Label)
-        self.view.addSubview(resposta3View)
-        resposta3View.addSubview(resposta3Label)
-        self.view.addSubview(resposta4View)
-        resposta4View.addSubview(resposta4Label)
+        self.view.addSubview(resposta1Button)
+        
+        self.view.addSubview(resposta2Button)
+        
+        self.view.addSubview(resposta3Button)
+        
+        self.view.addSubview(resposta4Button)
+        
         self.view.addSubview(centralizadorView)
         view.sendSubviewToBack(centralizadorView)
         
@@ -79,31 +70,33 @@ class Game1ViewController: UIViewController {
         resultadoView.backgroundColor = .systemPurple
         resultadoView.layer.cornerRadius = 27
         
-        resposta1View.backgroundColor = .systemPurple
-        resposta1View.layer.cornerRadius = 27
+        resposta1Button.backgroundColor = .systemPurple
+        resposta1Button.layer.cornerRadius = 27
+        resposta1Button.setTitle("1", for: .normal)
+        resposta1Button.tag = 1
+        resposta1Button.addTarget(self, action: #selector(resposta(_:)), for: .touchUpInside)
+        
 //        resposta1Label.text = "1"
-        resposta1Label.font = UIFont.systemFont(ofSize: 40)
-        resposta1Label.textAlignment = .center
-        resposta1Label.textColor = .white
         
         
-        resposta2View.backgroundColor = .systemPurple
-        resposta2View.layer.cornerRadius = 27
+        
+        resposta2Button.backgroundColor = .systemPurple
+        resposta2Button.layer.cornerRadius = 27
 //        resposta2Label.text = "1"
-        resposta2Label.font = UIFont.systemFont(ofSize: 50)
-        resposta2Label.textColor = .white
+        resposta2Button.tag = 2
+        resposta2Button.addTarget(self, action: #selector(resposta(_:)), for: .touchUpInside)
         
-        resposta3View.backgroundColor = .systemPurple
-        resposta3View.layer.cornerRadius = 27
+        resposta3Button.backgroundColor = .systemPurple
+        resposta3Button.layer.cornerRadius = 27
 //        resposta3Label.text = "1"
-        resposta3Label.font = UIFont.systemFont(ofSize: 50)
-        resposta3Label.textColor = .white
+        resposta3Button.tag = 3
+        resposta3Button.addTarget(self, action: #selector(resposta(_:)), for: .touchUpInside)
         
-        resposta4View.backgroundColor = .systemPurple
-        resposta4View.layer.cornerRadius = 27
+        resposta4Button.backgroundColor = .systemPurple
+        resposta4Button.layer.cornerRadius = 27
 //        resposta4Label.text = "1"
-        resposta4Label.font = UIFont.systemFont(ofSize: 50)
-        resposta4Label.textColor = .white
+        resposta4Button.tag = 4
+        resposta4Button.addTarget(self, action: #selector(resposta(_:)), for: .touchUpInside)
         
         
         faseImage.translatesAutoresizingMaskIntoConstraints = false
@@ -112,14 +105,10 @@ class Game1ViewController: UIViewController {
         desafioLabel.translatesAutoresizingMaskIntoConstraints = false
         igualLabel.translatesAutoresizingMaskIntoConstraints = false
         resultadoView.translatesAutoresizingMaskIntoConstraints = false
-        resposta1View.translatesAutoresizingMaskIntoConstraints = false
-        resposta1Label.translatesAutoresizingMaskIntoConstraints = false
-        resposta2View.translatesAutoresizingMaskIntoConstraints = false
-        resposta2Label.translatesAutoresizingMaskIntoConstraints = false
-        resposta3View.translatesAutoresizingMaskIntoConstraints = false
-        resposta3Label.translatesAutoresizingMaskIntoConstraints = false
-        resposta4View.translatesAutoresizingMaskIntoConstraints = false
-        resposta4Label.translatesAutoresizingMaskIntoConstraints = false
+        resposta1Button.translatesAutoresizingMaskIntoConstraints = false
+        resposta2Button.translatesAutoresizingMaskIntoConstraints = false
+        resposta3Button.translatesAutoresizingMaskIntoConstraints = false
+        resposta4Button.translatesAutoresizingMaskIntoConstraints = false
         centralizadorView.translatesAutoresizingMaskIntoConstraints = false
         
         
@@ -155,53 +144,48 @@ class Game1ViewController: UIViewController {
             centralizadorView.heightAnchor.constraint(equalToConstant: 240),
             centralizadorView.topAnchor.constraint(equalTo: desafioView.bottomAnchor, constant: 160),
             
-            resposta1View.widthAnchor.constraint(equalToConstant: 110),
-            resposta1View.heightAnchor.constraint(equalToConstant: 110),
-            resposta1View.topAnchor.constraint(equalTo: centralizadorView.topAnchor),
-            resposta1View.leftAnchor.constraint(equalTo: centralizadorView.leftAnchor),
+            resposta1Button.widthAnchor.constraint(equalToConstant: 110),
+            resposta1Button.heightAnchor.constraint(equalToConstant: 110),
+            resposta1Button.topAnchor.constraint(equalTo: centralizadorView.topAnchor),
+            resposta1Button.leftAnchor.constraint(equalTo: centralizadorView.leftAnchor),
             
             
-            resposta1Label.widthAnchor.constraint(equalToConstant: 40),
-            resposta1Label.heightAnchor.constraint(equalToConstant: 40),
-            resposta1Label.centerXAnchor.constraint(equalTo: resposta1View.centerXAnchor),
-            resposta1Label.centerYAnchor.constraint(equalTo: resposta1View.centerYAnchor),
-            
-            resposta2View.widthAnchor.constraint(equalToConstant: 110),
-            resposta2View.heightAnchor.constraint(equalToConstant: 110),
-            resposta2View.topAnchor.constraint(equalTo: centralizadorView.topAnchor),
-            resposta2View.rightAnchor.constraint(equalTo: centralizadorView.rightAnchor),
             
             
-            resposta2Label.widthAnchor.constraint(equalToConstant: 40),
-            resposta2Label.heightAnchor.constraint(equalToConstant: 40),
-            resposta2Label.centerXAnchor.constraint(equalTo: resposta2View.centerXAnchor),
-            resposta2Label.centerYAnchor.constraint(equalTo: resposta2View.centerYAnchor),
+            resposta2Button.widthAnchor.constraint(equalToConstant: 110),
+            resposta2Button.heightAnchor.constraint(equalToConstant: 110),
+            resposta2Button.topAnchor.constraint(equalTo: centralizadorView.topAnchor),
+            resposta2Button.rightAnchor.constraint(equalTo: centralizadorView.rightAnchor),
             
-            resposta3View.widthAnchor.constraint(equalToConstant: 110),
-            resposta3View.heightAnchor.constraint(equalToConstant: 110),
-            resposta3View.bottomAnchor.constraint(equalTo: centralizadorView.bottomAnchor),
-            resposta3View.leftAnchor.constraint(equalTo: centralizadorView.leftAnchor),
             
-            resposta3Label.widthAnchor.constraint(equalToConstant: 40),
-            resposta3Label.heightAnchor.constraint(equalToConstant: 40),
-            resposta3Label.centerXAnchor.constraint(equalTo: resposta3View.centerXAnchor),
-            resposta3Label.centerYAnchor.constraint(equalTo: resposta3View.centerYAnchor),
             
-            resposta4View.widthAnchor.constraint(equalToConstant: 110),
-            resposta4View.heightAnchor.constraint(equalToConstant: 110),
-            resposta4View.bottomAnchor.constraint(equalTo: centralizadorView.bottomAnchor),
-            resposta4View.rightAnchor.constraint(equalTo: centralizadorView.rightAnchor),
+            resposta3Button.widthAnchor.constraint(equalToConstant: 110),
+            resposta3Button.heightAnchor.constraint(equalToConstant: 110),
+            resposta3Button.bottomAnchor.constraint(equalTo: centralizadorView.bottomAnchor),
+            resposta3Button.leftAnchor.constraint(equalTo: centralizadorView.leftAnchor),
             
-            resposta4Label.widthAnchor.constraint(equalToConstant: 40),
-            resposta4Label.heightAnchor.constraint(equalToConstant: 40),
-            resposta4Label.centerXAnchor.constraint(equalTo: resposta4View.centerXAnchor),
-            resposta4Label.centerYAnchor.constraint(equalTo: resposta4View.centerYAnchor),
+            
+            resposta4Button.widthAnchor.constraint(equalToConstant: 110),
+            resposta4Button.heightAnchor.constraint(equalToConstant: 110),
+            resposta4Button.bottomAnchor.constraint(equalTo: centralizadorView.bottomAnchor),
+            resposta4Button.rightAnchor.constraint(equalTo: centralizadorView.rightAnchor)
         ])
         
-        
+        self.faseImage = faseImage
+        self.faseLabel = faseLabel
+        self.desafioView = desafioView
+        self.desafioLabel = desafioLabel
+        self.igualLabel = igualLabel
+        self.resultadoView = resultadoView
+        self.resposta1Button = resposta1Button
+        self.resposta2Button = resposta2Button
+        self.resposta3Button = resposta3Button
+        self.resposta4Button = resposta4Button
+        self.centralizadorView = centralizadorView
     }
-    
-    
+    @objc func resposta(_ sender: UIButton){
+//        print(sender.tag)
+    }
    
 }
 
